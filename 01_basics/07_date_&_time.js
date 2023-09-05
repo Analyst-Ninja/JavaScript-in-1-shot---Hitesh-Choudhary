51e33127d38afaf3948366a92514da11b8b9e755
Analyst-Ninja <r.kumar01@hotmail.com>
2023-09-05 21:20:00 +0530

    date & time

---
+++ 01_basics/07_date_&_time.js
@@ -0,0 +1,9 @@
+// Data
+let myDate = new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toUTCString());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.toJSON());
